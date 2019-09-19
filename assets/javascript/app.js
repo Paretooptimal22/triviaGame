5ac8e9ffab46cb60929e2edb39a31fc0e21cc310
Paretooptimal22 <you.k2208@gmail.com>
2019-09-18 22:51:55 -0700

    created format of app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,25 @@
-const questions = [
-  landmark1 = {
-    choices: [`Basilica La Sagrada Familia`, `Santiago de Compostela Cathedral`, `Santa Iglesia Catedral Primada de Toledo`, `Catedral de Sevilla`]
-  }
-  landmark2 = {
-    choices: [`The Louvre`, `Arc de Triomphe`, `Guggenheim Museum`, `Grand Palais`]
-  }
-  landmark3 = {
-    choices: [`Eiffel Tower`, `Tokyo Tower`, `CN Tower`, `Space Needle`]
-  }
-  landmark4 = {
-    choices: [`The Woolworth Building`, `Chrysler Building`, `Flatiron Building`, `Lloyd's Building`]
-  }
-  landmark5 = {
-    choices: [`Sydney Opera House`, `Metropolitan Opera House`, `Melbourne Opera House`, `Christchurch Opera`]
-  }
-  landmark6 = {
-    choices: [`Marina Bay Sands`, `The Mira`, `Yas Hotel`, `W Barcelona`]
-  }
-  landmark7 = {
-    choices: [`Tower Bridge`, `Twin Bridge`, `London Bridge`, `Charles Bridge`]
-  }
-  landmark8 = {
-    choices: [`Pantheon`, `Roman Forum`, `Duomo di Milano`, `Capri`]
-  }
-  landmark9 = {
-    choices: [`Brandenburg Gate`, `Arc de Triomphe`, `Merdian Gate`, `Golden Gate`]
-  }
-  landmark10 = {
-    choices: [`Hagia Sophia`, `Alhambra`, `Mosque of Cordoba`, `Saint Basil's Cathedral`]
-  }
-]
+document.addEventListener(`load`, )
+
+const questions = () => {
+  landmark1 = [`Basilica La Sagrada Familia`, `Santiago de Compostela Cathedral`, `Santa Iglesia Catedral Primada de Toledo`, `Catedral de Sevilla`]
+  
+  landmark2 = [`The Louvre`, `Arc de Triomphe`, `Guggenheim Museum`, `Grand Palais`]
+  
+  landmark3 = [`Eiffel Tower`, `Tokyo Tower`, `CN Tower`, `Space Needle`]
+  
+  landmark4 = [`The Woolworth Building`, `Chrysler Building`, `Flatiron Building`, `Lloyd's Building`]
+  
+  landmark5 = [`Sydney Opera House`, `Metropolitan Opera House`, `Melbourne Opera House`, `Christchurch Opera`]
+  
+  landmark6 = [`Marina Bay Sands`, `The Mira`, `Yas Hotel`, `W Barcelona`]
+  
+  landmark7 = [`Tower Bridge`, `Twin Bridge`, `London Bridge`, `Charles Bridge`]
+  
+  landmark8 = [`Pantheon`, `Roman Forum`, `Duomo di Milano`, `Capri`]
+  
+  landmark9 = [`Brandenburg Gate`, `Arc de Triomphe`, `Merdian Gate`, `Golden Gate`]
+  
+  landmark10 = [`Hagia Sophia`, `Alhambra`, `Mosque of Cordoba`, `Saint Basil's Cathedral`]
+
+
+}
