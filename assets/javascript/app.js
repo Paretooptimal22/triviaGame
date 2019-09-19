84b8f8bb279d593f9a137cd3635190a395c12010
Paretooptimal22 <you.k2208@gmail.com>
2019-09-18 20:08:06 -0700

    added images, bulma cdn, array

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+const questions = [
+  landmark1 = {
+    choices: [`Basilica La Sagrada Familia`, `Santiago de Compostela Cathedral`, `Santa Iglesia Catedral Primada de Toledo`, `Catedral de Sevilla`]
+  }
+  landmark2 = {
+    choices: [`The Louvre`, `Arc de Triomphe`, `Guggenheim Museum`, `Grand Palais`]
+  }
+  landmark3 = {
+    choices: [`Eiffel Tower`, `Tokyo Tower`, `CN Tower`, `Space Needle`]
+  }
+  landmark4 = {
+    choices: [`The Woolworth Building`, `Chrysler Building`, `Flatiron Building`, `Lloyd's Building`]
+  }
+  landmark5 = {
+    choices: [`Sydney Opera House`, `Metropolitan Opera House`, `Melbourne Opera House`, `Christchurch Opera`]
+  }
+  landmark6 = {
+    choices: [`Marina Bay Sands`, `The Mira`, `Yas Hotel`, `W Barcelona`]
+  }
+]
